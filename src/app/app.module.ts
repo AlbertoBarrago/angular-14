34eeaec674053e45fe7fa9c8317ae18d6303ea07
Alberto Barrago <a.barrago69@gmail.com>
2022-06-15 15:47:51 +0200

    added footer --sa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HeaderComponent} from "./components/header/header.component";
+import {HeaderComponent} from "./components/standalone/header/header.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FooterComponent} from "./components/standalone/footer/footer.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HeaderComponent,
+    FooterComponent,
     BrowserAnimationsModule
   ],
   providers: [],
---
+++ src/app/components/standalone/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+  year: Date = new Date;
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
